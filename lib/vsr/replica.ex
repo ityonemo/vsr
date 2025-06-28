ae6d4b059a26086bc408fb0a3daf889ecc173fc4
Fly Dev <dev@fly.io>
2025-06-28 18:43:05 +0000

    fix blocking behavior to actually block operations

--- lib/vsr/replica.ex
+++ lib/vsr/replica.ex
@@ -186,6 +186,46 @@ defmodule Vsr.Replica do
         {:unblock, _} -> {:noreply, state}
       end
     else
+      {:noreply, state}
+    end
+  end
+  defp maybe_block_and_reply(state) do
+    if state.blocking do
+      receive do
+        {:unblock, _} -> {:noreply, state}
+      end
+    else
+      {:noreply, state}
+    end
+  end
+  defp maybe_block_and_reply(state) do
+    if state.blocking do
+      receive do
+        {:unblock, _} -> {:noreply, state}
+      end
+    else
+      {:noreply, state}
+    end
+  end
+  defp maybe_block_and_reply(state) do
+    if state.blocking do
+      receive do
+        {:unblock, _} -> {:noreply, state}
+      end
+    else
+      {:noreply, state}
+    end
+  end
+  defp maybe_block_and_reply(state) do
+    if state.blocking do
+      receive do
+        {:unblock, _} -> {:noreply, state}
+      end
+    else
+      {:noreply, state}
+    end
+  end
+    else
       # Reply sent via GenServer call mechanism
       {:noreply, state}
     end
