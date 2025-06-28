474deaf95196a9a2e0f2edecadd9a8ed17c06326
Fly Dev <dev@fly.io>
2025-06-28 20:59:23 +0000

    fix log protocol to only declare function signatures

--- lib/vsr/log.ex
+++ lib/vsr/log.ex
@@ -17,58 +17,63 @@ defmodule Vsr.Log do
   @doc """
   Create a new log instance.
   """
-  @spec new(keyword()) :: t()
-  def new(opts \\ [])
+  @callback new(keyword()) :: t()
 
   @doc """
   Append an entry to the log.
 
   Returns the updated log.
   """
-  @spec append(t(), view(), op_number(), operation(), sender_id()) :: t()
-  def append(log, view, op_number, operation, sender_id)
+  @callback append(t(), view(), op_number(), operation(), sender_id()) :: t()
 
   @doc """
   Get an entry at the specified operation number.
 
   Returns `{:ok, entry}` if found, `{:error, :not_found}` otherwise.
   """
-  @spec get(t(), op_number()) :: {:ok, log_entry()} | {:error, :not_found}
-  def get(log, op_number)
+  @callback get(t(), op_number()) :: {:ok, log_entry()} | {:error, :not_found}
 
   @doc """
   Get all entries in the log.
 
   Returns a list of log entries in order.
   """
-  @spec get_all(t()) :: [log_entry()]
-  def get_all(log)
+  @callback get_all(t()) :: [log_entry()]
 
   @doc """
   Get entries from the specified operation number onwards.
 
   Returns a list of log entries from op_number to the end.
   """
-  @spec get_from(t(), op_number()) :: [log_entry()]
-  def get_from(log, op_number)
+  @callback get_from(t(), op_number()) :: [log_entry()]
 
   @doc """
   Get the length of the log (number of entries).
   """
-  @spec length(t()) :: non_neg_integer()
-  def length(log)
+  @callback length(t()) :: non_neg_integer()
 
   @doc """
   Replace the entire log with new entries.
 
   Used during state transfer and view changes.
   """
-  @spec replace(t(), [log_entry()]) :: t()
-  def replace(log, entries)
+  @callback replace(t(), [log_entry()]) :: t()
 
   @doc """
   Clear all entries from the log.
   """
-  @spec clear(t()) :: t()
-  def clear(log)
+  @callback clear(t()) :: t()
+
+  # Convenience functions that delegate to the implementation
+  def new(log_impl, opts \\ []), do: log_impl.new(opts)
+
+  def append(log, view, op_number, operation, sender_id),
+    do: log.__struct__.append(log, view, op_number, operation, sender_id)
+
+  def get(log, op_number), do: log.__struct__.get(log, op_number)
+  def get_all(log), do: log.__struct__.get_all(log)
+  def get_from(log, op_number), do: log.__struct__.get_from(log, op_number)
+  def length(log), do: log.__struct__.length(log)
+  def replace(log, entries), do: log.__struct__.replace(log, entries)
+  def clear(log), do: log.__struct__.clear(log)
 end
