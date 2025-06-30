38b5c92c8a0cf53711d6667e22f530452056d8f7
Fly Dev <dev@fly.io>
2025-06-30 15:50:05 +0000

    remove missing application module reference

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Vsr.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Vsr.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
